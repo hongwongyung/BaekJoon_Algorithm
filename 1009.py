65c430d8e6242c533f93271ef4c24491223defec
hongwongyung <84018359+hongwongyung@users.noreply.github.com>
2021-12-28 23:48:33 +0900

    백준 1009번 문제

---
+++ 1009.py
@@ -0,0 +1,14 @@
+num = int(input())
+result = []
+list = [[10],[1],[6,2,4,8],[1,3,9,7],[6,4],[5],[6],[1,7,9,3],[6,8,4,2],[1,9]]
+for _ in range(num):
+    a, b = map(int, input().split())
+    a = a%10
+    if a == 0 or a == 1 or a == 5 or a == 6:
+        result.append(list[a][0])
+    elif a == 4 or a == 9:
+        result.append(list[a][b%2])
+    else:
+        result.append(list[a][b%4])
+for i in range(num):
+    print(result[i])
